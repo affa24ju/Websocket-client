c81b725117777307cfa626b7a0f4c31b409fcb9d
Fatema Afroza <fafroza2017@gmail.com>
2025-08-26 12:40:21 +0200

    Lagt till chat funktionen

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Hej från script.js!");
 // Back-end körs på http://localhost:8080 och WebSocket-endpoint är /websocket inne i configurationen
 const socket = new SockJS("http://localhost:8080/websocket");
 const messageList = document.getElementById("messageList");
+const sendText = document.getElementById("sendText");
 const sendBtn = document.getElementById("sendBtn");
 
 const stompClient = Stomp.over(socket);
@@ -18,11 +19,28 @@ stompClient.connect({}, (frame) => {
         let li = document.createElement("li");
         li.innerText = JSON.parse(greeting.body).content;
         messageList.appendChild(li);
-        
+ 
     })
-    sendHello("Från klienten: Tania "); // Skicka ett meddelande när anslutningen är upprättad  
+
+    // Prenumrera på chat-meddelanden
+    stompClient.subscribe('/topic/chat', (chat) => { 
+        console.log("Chat: ", JSON.parse(chat.body).chat);
+
+        let li = document.createElement("li");
+        li.innerText = JSON.parse(chat.body).chat;
+        messageList.appendChild(li);
+    })
+
+    sendHello("Tania ");
+ 
 })
 // Använder name från back-end, inne i HelloMessage-klassen
 function sendHello(name) {
     stompClient.send("/app/hello", {}, JSON.stringify({'name': name}));
 }
+// Skicka meddelande till backend när knappen klickass
+sendBtn.addEventListener("click", () => {
+    // Skapa /chat endpoint i backend
+    stompClient.send("/app/chat", {}, JSON.stringify({'content': sendText.value}));
+    sendText.value = ""; // Rensa input-fältet
+})
