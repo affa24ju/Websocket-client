89388fd10edaa7d609833806c57b71b57a714751
Fatema Afroza <fafroza2017@gmail.com>
2025-08-26 11:50:20 +0200

    Funktion som visar meddelande på sidan

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ console.log("Hej från script.js!");
 // Anslut till WebSocket-servern
 // Back-end körs på http://localhost:8080 och WebSocket-endpoint är /websocket inne i configurationen
 const socket = new SockJS("http://localhost:8080/websocket");
+const messageList = document.getElementById("messageList");
+const sendBtn = document.getElementById("sendBtn");
 
 const stompClient = Stomp.over(socket);
 
 stompClient.connect({}, (frame) => { 
-    console.log('Connected: ' + frame);    
+    console.log('Connected: ' + frame);  
+
+    // Prenumerera på meddelanden från servern
+    stompClient.subscribe('/topic/greetings', (greeting) => { 
+        console.log("Greeting: ", JSON.parse(greeting.body).content);
+
+        // Visa meddelandet som en lista till klienten
+        let li = document.createElement("li");
+        li.innerText = JSON.parse(greeting.body).content;
+        messageList.appendChild(li);
+        
+    })
+    sendHello("Från klienten: Tania "); // Skicka ett meddelande när anslutningen är upprättad  
 })
+// Använder name från back-end, inne i HelloMessage-klassen
+function sendHello(name) {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': name}));
+}
